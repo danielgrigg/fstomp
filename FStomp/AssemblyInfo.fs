96f1e24130fb74cdf77f5429e7f8105cf09c29f9
Daniel Casimir Grigg <daniel.grigg@covata.com>
2014-07-25 14:26:45 +1000

    Add assemblyinfo.cs

---
+++ FStomp/AssemblyInfo.fs
@@ -0,0 +1,19 @@
+﻿module fstomp.AssemblyInfo
+
+open System
+open System.Reflection;
+open System.Runtime.InteropServices;
+ 
+[<assembly: AssemblyTitle("fstomp")>]
+[<assembly: AssemblyDescription("Stomp protocol parser and formatter")>]
+[<assembly: AssemblyCompany("Daniel Casimir Grigg")>]
+[<assembly: AssemblyProduct("fstomp")>]
+[<assembly: AssemblyCopyright("Copyright © 2014 Daniel Casimir Grigg")>]
+[<assembly: AssemblyTrademark("fstomp")>]
+[<assembly: Guid("90CE44C3-8BF2-430C-8FF2-555CE0585596")>]
+[<assembly: ComVisible(false)>]
+[<assembly: CLSCompliant(false)>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do()
